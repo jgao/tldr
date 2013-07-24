9f8e4cc4211edfcc4a6e1fbaff51a451d2e50f94
Jack Gao <jackgao2006@gmail.com>
2013-07-24 00:18:22 -0400

    added conciseIndex to ajax call

--- generator/generator.js
+++ generator/generator.js
@@ -145,5 +145,90 @@ var generate = function(){
   return [ summary, count, concisenessSingle, sentences.length ];
 }
 
+var summarizeDemoDay = function(story, sources){
+  var articles = [];
+  var sentences = [];
+  for(var i=0;i<sources.length;i++){
+    var file = "data/" + story + sources[i];
+    articles[i] = fs.readFileSync(file, "utf8");
+    sentences[i] = articles[i].split(".");
+  }
+
+  var freq = new HashMap();
+
+  for(var k=0;k<sentences.length;k++){
+    for(var i=0;i<sentences[k].length;i++){
+      var words = sentences[k][i].split(" "); // split just by " " for now
+      for(var j=0;j<words.length;j++){
+        var count = freq.get(words[j].toLowerCase());
+        count = count ? count+1 : 1;
+        freq.set(words[j].toLowerCase(), count);
+      }
+    }
+  }
+
+  for(var i=0;i<commonWords.length;i++){
+    freq.set(commonWords[i].toLowerCase(), 1);
+  }
+
+  var sentenceScoresList = [];
+
+  for(var i=0;i<sentences1.length;i++){
+    var words = sentences1[i].split(" "); // split just by " " for now
+    sentenceScores1[i] = 0;
+    if(sentences1[i].indexOf("\"") < 0){
+      if(sentences1[i].indexOf("\n") >= 0) sentences1[i] = sentences1[i].split("\n").pop(); // removes titles
+      for(var j=0;j<words.length;j++){
+      sentenceScores1[i] += freq.get(words[j].toLowerCase());
+      }
+      sentenceScores1[i] = sentenceScores1[i]/words.length;
+    }
+  }
+  for(var i=0;i<sentences2.length;i++){
+    var words = sentences2[i].split(" "); // split just by " " for now
+    sentenceScores2[i] = 0;
+    if(sentences2[i].indexOf("\"") < 0){
+      if(sentences2[i].indexOf("\n") >= 0) sentences2[i] = sentences2[i].split("\n").pop(); // removes titles
+      for(var j=0;j<words.length;j++){
+      sentenceScores2[i] += freq.get(words[j].toLowerCase());
+      }
+      sentenceScores2[i] = sentenceScores2[i]/words.length;
+    }
+  }
+
+  var sortedScores = sentenceScores1.slice().concat(sentenceScores2.slice()).sort(function(a, b){
+      return b-a;
+  });
+
+  var threashold = sortedScores[Math.floor((sentenceScores1.length+sentenceScores2.length)*concisenessMultiple)-1];
+
+  var count1 = 0;
+  var count2 = 0;
+
+  var summary = "";
+  for(var i=0;i<Math.max(sentences1.length, sentences2.length);i++){
+    if(i < sentences1.length){
+      if(sentenceScores1[i] >= threashold){
+      summary += sentences1[i] + ". ";
+      count1++;
+      }
+    }
+    if(i < sentences2.length){
+      if(sentenceScores2[i] >= threashold){
+      summary += sentences2[i] + ". ";
+      count2++;
+      }
+    }
+  }
+
+  return [ summary, count1, count2, concisenessMultiple, sentences1.length, sentences2.length ];
+}
+
+
+
+
 exports.generate = generate;
 exports.generateMultiple = generateMultiple;
+
+// for demo day only
+exports.summarizeDemoDay(story, sources);
--- routes/summarizer.js
+++ routes/summarizer.js
@@ -5,12 +5,25 @@ var summarize = function(req, res){
 
 	var story = req.query.story;
 	var sources = req.query.sources;
+	var conciseIndex = req.query.conciseIndex;
 
+	var title = getTitle(story);
+	var summary = generator.summarizeDemoDay(story, sources, conciseIndex); // demo day version
+	
 	res.contentType("json");
 	res.send({
-		title: "Royal Baby is Born - It's a Boy!",
-		summary: "HERE IS SOME SUMMARY TO BE GENERATED"
+		title: title,
+		summary: summary
 	});
+}
+
+var getTitle(story){
+	if(story == "chineseearthquakebbc") return "Earthquake Devastates Chinese Town";
+	if(story == "royalbaby") return "Royal Baby is Born - It's a Boy!";
+	if(story == "trainexplosion") return "Train Derails and Explodes in Quebec, Death Toll Rising";
+	if(story == "weiner") return "New York City Mayor Candidate Weiner Caught in Another Scandal";
+
+	return "Please stop messing with the javascript. STAHP.";
 }
 
 exports.summarize = summarize;
