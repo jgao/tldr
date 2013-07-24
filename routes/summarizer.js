f535b0ffcbe3ef6944e72fda934832c198f3d50d
Jack Gao <jackgao2006@gmail.com>
2013-07-23 23:51:45 -0400

    i r bad at grammar

--- routes/summarizer.js
+++ routes/summarizer.js
@@ -1,5 +1,10 @@
+var generator = require("../generator/generator");
+
 var summarize = function(req, res){
 	console.log(req.query);
+
+	var story = req.query.story;
+	var sources = req.query.sources;
 
 	res.contentType("json");
 	res.send({
