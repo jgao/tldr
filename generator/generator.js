892771d30f51623dee1213a17440909c3a89ac4e
Jack Gao <jackgao2006@gmail.com>
2013-07-24 01:16:10 -0400

    finalized api. see ajaxtester.html

--- generator/generator.js
+++ generator/generator.js
@@ -145,7 +145,7 @@ var generate = function(){
   return [ summary, count, concisenessSingle, sentences.length ];
 }
 
-var summarizeDemoDay = function(story, sources){
+var summarizeDemoDay = function(story, sources, concisenessIndex){
   var articles = [];
   var sentences = [];
   for(var i=0;i<sources.length;i++){
@@ -173,55 +173,48 @@ var summarizeDemoDay = function(story, sources){
 
   var sentenceScoresList = [];
 
-  for(var i=0;i<sentences1.length;i++){
-    var words = sentences1[i].split(" "); // split just by " " for now
-    sentenceScores1[i] = 0;
-    if(sentences1[i].indexOf("\"") < 0){
-      if(sentences1[i].indexOf("\n") >= 0) sentences1[i] = sentences1[i].split("\n").pop(); // removes titles
-      for(var j=0;j<words.length;j++){
-      sentenceScores1[i] += freq.get(words[j].toLowerCase());
+  for(var k=0;k<sentences.length;k++){
+    sentenceScoresList[k] = [];
+    for(var i=0;i<sentences[k].length;i++){
+      var words = sentences[k][i].split(" "); // split just by " " for now
+      sentenceScoresList[k][i] = 0;
+      if(sentences[k][i].indexOf("\"") < 0){
+        if(sentences[k][i].indexOf("\n") >= 0) sentences[k][i] = sentences[k][i].split("\n").pop(); // removes titles
+        for(var j=0;j<words.length;j++){
+          sentenceScoresList[k][i] += freq.get(words[j].toLowerCase());
+        }
+        sentenceScoresList[k][i] = sentenceScoresList[k][i]/words.length;
       }
-      sentenceScores1[i] = sentenceScores1[i]/words.length;
-    }
-  }
-  for(var i=0;i<sentences2.length;i++){
-    var words = sentences2[i].split(" "); // split just by " " for now
-    sentenceScores2[i] = 0;
-    if(sentences2[i].indexOf("\"") < 0){
-      if(sentences2[i].indexOf("\n") >= 0) sentences2[i] = sentences2[i].split("\n").pop(); // removes titles
-      for(var j=0;j<words.length;j++){
-      sentenceScores2[i] += freq.get(words[j].toLowerCase());
-      }
-      sentenceScores2[i] = sentenceScores2[i]/words.length;
     }
   }
 
-  var sortedScores = sentenceScores1.slice().concat(sentenceScores2.slice()).sort(function(a, b){
+  var sortedScores = [];
+  var totalSentences = 0;
+  var maxSentences = -1;
+  for(var i=0;i<sentenceScoresList.length;i++){
+    totalSentences += sentences[i].length;
+    maxSentences = Math.max(maxSentences, sentences[i].length);
+    sortedScores = sortedScores.concat(sentenceScoresList[i].slice());
+  }
+
+  sortedScores = sortedScores.sort(function(a, b){
       return b-a;
   });
 
-  var threashold = sortedScores[Math.floor((sentenceScores1.length+sentenceScores2.length)*concisenessMultiple)-1];
-
-  var count1 = 0;
-  var count2 = 0;
+  var threashold = sortedScores[Math.floor(totalSentences * concisenessIndex) - 1];
 
   var summary = "";
-  for(var i=0;i<Math.max(sentences1.length, sentences2.length);i++){
-    if(i < sentences1.length){
-      if(sentenceScores1[i] >= threashold){
-      summary += sentences1[i] + ". ";
-      count1++;
-      }
-    }
-    if(i < sentences2.length){
-      if(sentenceScores2[i] >= threashold){
-      summary += sentences2[i] + ". ";
-      count2++;
+  for(var i=0;i<maxSentences;i++){
+    for(var j=0;j<sentences.length;j++){
+      if(i < sentences[j].length){
+        if(sentenceScoresList[j][i] >= threashold){
+          summary += sentences[j][i] + ". ";
+        }
       }
     }
   }
 
-  return [ summary, count1, count2, concisenessMultiple, sentences1.length, sentences2.length ];
+  return summary;
 }
 
 
@@ -231,4 +224,4 @@ exports.generate = generate;
 exports.generateMultiple = generateMultiple;
 
 // for demo day only
-exports.summarizeDemoDay(story, sources);
+exports.summarizeDemoDay = summarizeDemoDay;
